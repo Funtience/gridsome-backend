b12d8c11af2f2453b4115cd59d5826a0851d8e25
Funtience <769558413@qq.com>
2021-06-07 21:21:11 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aadbee5e1707a4f36523ef6e236abd56'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '31ed24ed-6083-4b0b-bbe7-572673fb2791'
+};
